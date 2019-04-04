ab01204d57b55152dde1ac8721badcaf5e9924c2
Dan Collins <dcollins31@gmail.com>
2019-04-03 19:24:26 -0500

    Added customization to admin

--- packages/admin.py
+++ packages/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Package
 
-admin.site.register(Package)
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('destination', 'price', 'list_date', 'is_published')
+    list_display_links = ('destination',)
+    list_editable = ('is_published',)
+    search_fields = ('destination', 'description', 'price')
+admin.site.register(Package, PackageAdmin)
 
