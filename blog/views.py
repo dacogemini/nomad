92d36e8ca6668ed8e14fd864f2749f74e5670f37
Dan Collins <dcollins31@gmail.com>
2019-08-07 09:35:42 -0500

    Added blog model

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from blog.models import Post
+
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+    return render(request, "blog_detail.html", context)
--- nomad/settings.py
+++ nomad/settings.py
@@ -21,7 +21,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'packages.apps.PackagesConfig',
-    'agents.apps.AgentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -30,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Custom Apps
     'django.contrib.humanize',
+    'blog',
 ]
 
 MIDDLEWARE = [
