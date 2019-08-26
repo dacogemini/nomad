1f7a86b7a8b283df01811749c369eb3fd4a5e1c2
Dan Collins <dcollins31@gmail.com>
2019-08-25 22:31:03 -0500

    removed local_settings

--- nomad/settings.py
+++ nomad/settings.py
@@ -141,3 +141,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dcollins31@gmail.com'
 EMAIL_HOST_PASSWORD = 'froglegs2'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
